cde2193acc134af30b7ea874f7fca2b8f6024c03
Nicolas <nicobenpe@gmail.com>
2025-02-02 08:39:54 -0300

    elimino head

--- Game_boy.py
+++ Game_boy.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 df = pd.read_csv ("C:/Users/Usuario/Desktop/Escriotrio Nico/PYTHON/CODE/Datos parte 2/best-selling-gameboy.csv")
 
-df = df.head(n=15)
+
 
 # Dividiremos el DataFrame en dos.
 # Primero, dividiremos el DataFrame en dos partes: una para los juegos de Nintendo
@@ -14,7 +14,8 @@ df_game_boy_color = df.query("Platform == 'Game Boy Color'")
 df = df_game_boy.sort_values(by = 'Sales', ascending = False).iloc[:10]
 
 # Ordenamos por ventas de mayor a menor e indexamos las 10 primeras posiciones.
-df= df_game_boy_color.sort_values(by = 'Sales', ascending = False).iloc[:10]
+df = df_game_boy_color.sort_values(by = 'Sales', ascending = False).iloc[:10]
+
 
 
 print (df)
