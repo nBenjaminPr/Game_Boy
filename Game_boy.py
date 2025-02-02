5e826573cc6b894599494743c793151dc4c55e02
Nicolas <nicobenpe@gmail.com>
2025-02-02 08:45:06 -0300

    Conteo de Juegos

--- Game_boy.py
+++ Game_boy.py
@@ -5,17 +5,18 @@ df = pd.read_csv ("C:/Users/Usuario/Desktop/Escriotrio Nico/PYTHON/CODE/Datos pa
 
 
 # Dividiremos el DataFrame en dos.
-# Primero, dividiremos el DataFrame en dos partes: una para los juegos de Nintendo
+# Primero, dividiremos el DataFrame en dos partes: una para los juegos de Game Boy
 df_game_boy = df.query("Platform == 'Game Boy'")
 # otra para los juegos de color.
 df_game_boy_color = df.query("Platform == 'Game Boy Color'")
 
 # Ordenamos por ventas de mayor a menor e indexamos las 10 primeras posiciones.
-df = df_game_boy.sort_values(by = 'Sales', ascending = False).iloc[:10]
+"""df = df_game_boy.sort_values(by = 'Sales', ascending = False).iloc[:10]"""
 
 # Ordenamos por ventas de mayor a menor e indexamos las 10 primeras posiciones.
-df = df_game_boy_color.sort_values(by = 'Sales', ascending = False).iloc[:10]
+"""df = df_game_boy_color.sort_values(by = 'Sales', ascending = False).iloc[:10]"""
 
-
+df = df['Platform'].value_counts()
+#Hay más Juegos de Game boy que juegos a color
 
 print (df)
