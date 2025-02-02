07124db01319305b915a190a8a30feda34475e89
Nicolas <nicobenpe@gmail.com>
2025-02-02 08:35:19 -0300

    Ordenando por ventas

--- Game_boy.py
+++ Game_boy.py
@@ -2,3 +2,19 @@ import pandas as pd
 
 df = pd.read_csv ("C:/Users/Usuario/Desktop/Escriotrio Nico/PYTHON/CODE/Datos parte 2/best-selling-gameboy.csv")
 
+df = df.head(n=15)
+
+# Dividiremos el DataFrame en dos.
+# Primero, dividiremos el DataFrame en dos partes: una para los juegos de Nintendo
+df_game_boy = df.query("Platform == 'Game Boy'")
+# otra para los juegos de color.
+df_game_boy_color = df.query("Platform == 'Game Boy Color'")
+
+# Ordenamos por ventas de mayor a menor e indexamos las 10 primeras posiciones.
+df = df_game_boy.sort_values(by = 'Sales', ascending = False).iloc[:10]
+
+# Ordenamos por ventas de mayor a menor e indexamos las 10 primeras posiciones.
+df= df_game_boy_color.sort_values(by = 'Sales', ascending = False).iloc[:10]
+
+
+print (df)
